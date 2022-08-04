f73b48a31400fa8d52f21222c9a246301b8692dd
EmanRiziq <emanriziq@gmail.com>
2022-08-04 09:47:01 +0300

    Class 09

--- modules/weather.js
+++ modules/weather.js
@@ -6,7 +6,7 @@ async function handleWeather(req, res) {
         let lon = req.query.lon;
         let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`
         let response = await axios.get(url)
-        let data = response.data.map(item => new Forecast(item))
+        let data = response.data.data.map(item => new Forecast(item))
         console.log(data)
         res.send(data)
     }
