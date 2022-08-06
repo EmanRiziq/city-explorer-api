65fb5e9b445ea971b8777a353b3ce393c7deed53
EmanRiziq <emanriziq@gmail.com>
2022-08-06 15:53:50 +0300

    bugs fixing

--- modules/movies.js
+++ modules/movies.js
@@ -3,19 +3,27 @@
 require('dotenv').config();
 
 const axios = require('axios');
+const movieCashe = {};
 
 async function handleMovies(req, res) {
 
     let searchQuery = req.query.searchQuery;
-    const moviesUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
-    const moviesList = await axios.get(moviesUrl);
-    console.log(moviesList.data);
-    const movieArr = moviesList.data.results.map(day => new MoviesInfo(day));
+    if (movieCashe[searchQuery] !== undefined) {
+        res.send(movieCashe[searchQuery])
+    }
+    else {
+        const moviesUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${searchQuery}`;
+        const moviesList = await axios.get(moviesUrl);
+        // console.log(moviesList.data);
+        const movieArr = moviesList.data.results.map(day => new MoviesInfo(day));
+        movieCashe[searchQuery] = movieArr
 
-    try {
-        res.send(movieArr)
-    } catch (err) {
-        res.status(404).send('Movie not found');
+
+        try {
+            res.send(movieArr)
+        } catch (err) {
+            res.status(404).send('Movie not found');
+        }
     }
 }
 
@@ -23,7 +31,7 @@ class MoviesInfo {
     constructor(movie) {
         this.title = movie.original_title;
         this.date = movie.release_date;
-        this.path = movie.poster_path;
+        this.path = (movie.poster_path ? `http://image.tmdb.org/t/p/w500${movie.poster_path}` : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT21FY3SpqoXi4NcShIpKXZDVGzTsrPkiyCEA&usqp=CAU');
         this.des = movie.overview;
         this.rate = movie.vote_average;
     }
--- modules/weather.js
+++ modules/weather.js
@@ -1,27 +1,40 @@
+"use strict";
+
+require('dotenv').config();
 const axios = require('axios');
+const weatherCashe = {};
 
 async function handleWeather(req, res) {
-    try {
-        let lat = req.query.lat;
-        let lon = req.query.lon;
+
+    let lat = req.query.lat;
+    let lon = req.query.lon;
+    let searchQuery = req.query.searchQuery
+
+    if (weatherCashe[searchQuery] !== undefined) {
+        res.send(weatherCashe[searchQuery])
+    }
+    else {
+
         let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`
         let response = await axios.get(url)
-        let data = response.data.data.map(item => new Forecast(item))
-        console.log(data)
-        res.send(data)
-    }
-    catch (error) {
-        res.status(404).send('Weather not found');
+        try {
+            let data = response.data.data.map(item => new Forecast(item))
+            weatherCashe[searchQuery] = data
+            res.send(data)
+        }
+        catch (error) {
+            res.status(404).send('Weather not found');
 
+        }
     }
 }
 
 
 class Forecast {
-    constructor(datetime, description) {
-        this.date = datetime;
-        this.description = description;
+    constructor(day) {
+        this.date = day.valid_date;
+        this.description =`Low of ${day.low_temp}, high of ${day.max_temp} with ${ day.weather.description}`;
     }
 }
 
-module.exports = {handleWeather}
+module.exports = { handleWeather }
