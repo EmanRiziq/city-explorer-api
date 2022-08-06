3119cf2e9f5fe3922424efdf6fb221b60875e782
EmanRiziq <emanriziq@gmail.com>
2022-08-06 18:34:26 +0300

    update eslint2

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const app = express();
 app.use(cors());
 const axios = require('axios');
 const port = process.env.port || 3002;
-const {handleWeather} = require('./modules/weather');
-const {handleMovies} = require('./modules/movies');
+const { handleWeather } = require('./modules/weather');
+const { handleMovies } = require('./modules/movies');
 
 
 
 //home router
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.status(200).send(['hi', 'hello'])
 });
 
@@ -21,6 +21,6 @@ app.get('/weather', handleWeather)
 app.get('/movies', handleMovies)
 
 
-  app.listen(port, () => {
-    console.log('working port '+ port )
-  })
+app.listen(port, () => {
+  console.log('working port ' + port)
+})
